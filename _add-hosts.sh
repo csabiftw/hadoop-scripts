c3fe866d4f94b386342585d14cfa673b1f1e89b5
Csaba Szerzo <csabiftw@gmail.com>
2014-05-05 16:50:59 +0200

    master host setup

---
+++ _add-hosts.sh
@@ -0,0 +1,7 @@
+# add master and slaves to /etc/hosts
+sudo sh -c 'echo "10.0.0.40 master" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.41 slave1" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.42 slave2" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.43 slave3" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.44 slave4" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.45 slave5" >> /etc/hosts'
---
+++ make-master.sh
@@ -0,0 +1,15 @@
+./_add-hosts.sh
+
+# setup passwordless auth
+sudo sh -c "ssh-copy-id -i /home/hduser/.ssh/id_dsa.pub hduser@slave1"
+sudo sh -c "ssh-copy-id -i /home/hduser/.ssh/id_dsa.pub hduser@slave2"
+sudo sh -c "ssh-copy-id -i /home/hduser/.ssh/id_dsa.pub hduser@slave3"
+sudo sh -c "ssh-copy-id -i /home/hduser/.ssh/id_dsa.pub hduser@slave4"
+sudo sh -c "ssh-copy-id -i /home/hduser/.ssh/id_dsa.pub hduser@slave5"
+
+# add slaves to hadoop (keep localhost too in this case)
+sudo sh -c 'echo "slave1" >> /usr/local/hadoop/etc/hadoop/slaves'
+sudo sh -c 'echo "slave2" >> /usr/local/hadoop/etc/hadoop/slaves'
+sudo sh -c 'echo "slave3" >> /usr/local/hadoop/etc/hadoop/slaves'
+sudo sh -c 'echo "slave4" >> /usr/local/hadoop/etc/hadoop/slaves'
+sudo sh -c 'echo "slave5" >> /usr/local/hadoop/etc/hadoop/slaves'
