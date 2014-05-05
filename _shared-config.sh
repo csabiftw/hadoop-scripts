dffc6c54f7eb0fa87fd19ca5fd49d33b4196338c
Csaba Szerzo <csabiftw@gmail.com>
2014-05-05 19:14:19 +0200

    append fix

--- _shared-config.sh
+++ _shared-config.sh
@@ -3,7 +3,7 @@ mkdir /usr/local/hadoop/tmp
 cd /usr/local/hadoop/etc/hadoop
 
 # core-site.xml
-awk 'BEGIN{RS="</configuration>"}/<configuration>/{gsub(/<configuration>.*/,"<configuration><property><name>fs.default.name</name><value>hdfs://master:9000</value></property><property><name>hadoop.tmp.dir</name><value>/usr/local/hadoop/tmp</value></property></configuration>")}1' core-site.xml >> core-site.xml
+awk 'BEGIN{RS="</configuration>"}/<configuration>/{gsub(/<configuration>.*/,"<configuration><property><name>fs.default.name</name><value>hdfs://master:9000</value></property><property><name>hadoop.tmp.dir</name><value>/usr/local/hadoop/tmp</value></property></configuration>")}1' core-site.xml > core-site.xml
 
 # yarn-site.xml
 sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn.resourcemanager.resource-tracker.address\</name>\<value>master:8025\</value>\</property>\<property>\<name>yarn.resourcemanager.scheduler.address\</name>\<value>master:8030\</value>\</property>\<property>\<name>yarn.resourcemanager.address\</name>\<value>master:8040\</value>\</property>=g' yarn-site.xml 
