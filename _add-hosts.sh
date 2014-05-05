c5c3e54c5f79e57ef42821752c8f66324e09ab03
Csaba Szerzo <csabiftw@gmail.com>
2014-05-06 00:05:28 +0200

    hosts and format

--- _add-hosts.sh
+++ _add-hosts.sh
@@ -1,7 +1,7 @@
 # add master and slaves to /etc/hosts
-sudo sh -c 'echo "10.0.0.40 master" >> /etc/hosts'
-sudo sh -c 'echo "10.0.0.41 slave1" >> /etc/hosts'
-sudo sh -c 'echo "10.0.0.42 slave2" >> /etc/hosts'
-sudo sh -c 'echo "10.0.0.43 slave3" >> /etc/hosts'
-sudo sh -c 'echo "10.0.0.44 slave4" >> /etc/hosts'
-sudo sh -c 'echo "10.0.0.45 slave5" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.40 master hdp1" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.41 slave1 hdp2" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.42 slave2 hdp3" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.43 slave3 hdp4" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.44 slave4 hdp5" >> /etc/hosts'
+sudo sh -c 'echo "10.0.0.45 slave5 hdp6" >> /etc/hosts'
--- _clear-data-dir.sh
+++ _clear-data-dir.sh
@@ -1,3 +1,5 @@
+# run on master as hduser
+
 ssh master "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
 ssh slave1 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
 ssh slave2 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
@@ -5,3 +7,5 @@ ssh slave3 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/h
 ssh slave4 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
 ssh slave5 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
 ssh slave6 "rm -R /usr/local/hadoop/tmp/* ; rm -R ~/mydata/hdfs;mkdir ~/mydata/hdfs;mkdir ~/mydata/hdfs/namenode;mkdir ~/mydata/hdfs/datanode;ls -la mydata/hdfs/;"
+
+hadoop namenode -format
