a51059f9c0dcff520f7333d52ae686a1873c9073
Csaba Szerzo <csabiftw@gmail.com>
2014-05-05 23:36:46 +0200

    chmod

--- _shared-config.sh
+++ _shared-config.sh
@@ -4,4 +4,4 @@ sudo -u hduser  mkdir /usr/local/hadoop/tmp
 sudo -u hduser sh -c "./_awk-core-site.sh"
 
 # yarn-site.xml
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn.resourcemanager.resource-tracker.address\</name>\<value>master:8025\</value>\</property>\<property>\<name>yarn.resourcemanager.scheduler.address\</name>\<value>master:8030\</value>\</property>\<property>\<name>yarn.resourcemanager.address\</name>\<value>master:8040\</value>\</property>=g' /usr/local/hadoop/etc/hadoop/yarn-site.xml 
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn.resourcemanager.resource-tracker.address\</name>\<value>master:8025\</value>\</property>\<property>\<name>yarn.resourcemanager.scheduler.address\</name>\<value>master:8030\</value>\</property>\<property>\<name>yarn.resourcemanager.address\</name>\<value>master:8045\</value>\</property>=g' /usr/local/hadoop/etc/hadoop/yarn-site.xml 
